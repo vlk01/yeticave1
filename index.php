e789ee5ad1760b8a6e20377540db0c8cd888eb74
vlk01 <kapustin27@mail.ru>
2021-03-01 13:18:06 +0300

    Laba1

--- index.php
+++ index.php
@@ -27,7 +27,23 @@ $user_name = 'vlk01'; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
+            <?php if($is_auth): ?>
+                <div class="user-menu__image">
+                    <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
+                </div>
+                <div class="user-menu__logged">
+                    <p></p>
+                </div>
+            <?php else: ?>
+                <ul class="user-menu__list">
+                    <li class="user-menu__item">
+                        <a href="#">Регистрация</a>
+                    </li>
+                    <li class="user-menu__item">
+                        <a href="#">Вход</a>
+                    </li>
+                </ul>
+            <?php endif; ?>
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
 
         </nav>
