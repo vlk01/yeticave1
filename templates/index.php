1f6a19a2335330492f9cb53a32f0af30a170bd28
vlk01 <79832567+vlk01@users.noreply.github.com>
2021-03-03 21:26:38 +0300

    Практическая 5 - продолжение

--- templates/index.php
+++ templates/index.php
@@ -31,7 +31,7 @@
                                 <span class="lot__cost"> <?= Sum_Price($val['count'], true) ?><b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
-                                12:23
+                                 <?php echo(My_Timer()); ?>
                             </div>
                         </div>
                     </div>
