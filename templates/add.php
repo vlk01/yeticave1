988cf6c04dd88eb676de024590da4f6d23ceb14a
vlk01 <79832567+vlk01@users.noreply.github.com>
2021-03-24 16:20:47 +0300

    Практическая 10(4)

--- templates/add.php
+++ templates/add.php
@@ -10,15 +10,15 @@
         <?php endforeach; ?>
     </ul>
 </nav>
-<form class="form form--add-lot container form--invalid" action="adds.php" method="post"> <!-- form--invalid -->
+<form class="form form--add-lot container form--invalid <?= empty($errors)? "" : "form__item--invalid" ?>" action="adds.php" method="post"> <!-- form--invalid -->
     <h2>Добавление лота</h2>
     <div class="form__container-two">
-        <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
+         <div class="form__item form__item--invalid <?= empty($errors['lot-name'])? "" : "form__item--invalid" ?>"> <!-- form__item--invalid -->
             <label for="lot-name">Наименование <sup>*</sup></label>
             <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота">
             <span class="form__error">Введите наименование лота</span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?= empty($errors['category'])? "" : "form__item--invalid" ?>">
             <label for="category">Категория <sup>*</sup></label>
             <select id="category" name="category">
                 <option>Выберите категорию --</option>
@@ -30,7 +30,7 @@
             <span class="form__error">Выберите категорию</span>
         </div>
     </div>
-    <div class="form__item form__item--wide">
+    <div class="form__item form__item--wide <?= empty($errors['message'])? "" : "form__item--invalid" ?>">
         <label for="message">Описание <sup>*</sup></label>
         <textarea id="message" name="message" placeholder="Напишите описание лота"></textarea>
         <span class="form__error">Напишите описание лота</span>
@@ -45,22 +45,24 @@
         </div>
     </div>
     <div class="form__container-three">
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small <?= empty($errors['lot-rate'])? "" : "form__item--invalid" ?>" >
             <label for="lot-rate">Начальная цена <sup>*</sup></label>
             <input id="lot-rate" type="text" name="lot-rate" placeholder="0">
             <span class="form__error">Введите начальную цену</span>
         </div>
-        <div class="form__item form__item--small">
+        <div class="form__item form__item--small <?= empty($errors['lot-step'])? "" : "form__item--invalid" ?>">
             <label for="lot-step">Шаг ставки <sup>*</sup></label>
             <input id="lot-step" type="text" name="lot-step" placeholder="0">
             <span class="form__error">Введите шаг ставки</span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?= empty($errors['lot-date'])? "" : "form__item--invalid" ?>">
             <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
             <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
             <span class="form__error">Введите дату завершения торгов</span>
         </div>
     </div>
-
+<? if(!empty($errors)): ?>
+    <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
+    <? endif; ?>
     <button type="submit" class="button">Добавить лот</button>
 </form>
