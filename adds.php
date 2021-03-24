b10022eec25e8df9c7d1a1c9c6d2c06d182c49bb
vlk01 <79832567+vlk01@users.noreply.github.com>
2021-03-24 16:24:17 +0300

    Практическая 10(final)

--- adds.php
+++ adds.php
@@ -29,6 +29,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         }
     }
+	 if(isset($_FILES['lotPhotos'])){
+        $finfo = finfo_open(FILEINFO_MINE_TYPE);
+        $file_name = $_FILES['lotPhotos']['name'];
+        $file_path = __DIR__ . '/img/';
+        $file_tmpname = $_FILES['lotPhotos']['tmp_name'];
+        $file_type = finfo_file($finfo, $file_tmpname);
+        if($file_type == 'image/gif'){
+            move_uploaded_file($_FILES['lotPhotos']['tmp_name'], $file_path . $file_name);
+        }
+        $file_url = 'img/' . $file_name;
+    }
+
     if(count($errors) !== 0) {
         $page_content = compile_template('add.php',
             ['errors' => $errors,
@@ -44,12 +56,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 "description" => $_POST['message'],
                 "account_id" => $_SESSION['auth']['account_id']
         ];
-		$page_content = compile_template( 'add.php',
-            ['categories_list' => $categories_list,
-                'lot' => $currentLot,
-                'lotID' => $lotID,
-                'data_list' => $data_list,
-                'lot_time_remaining' => $lot_time_remaining]);
+		$page_content = compile_template('add.php',
+        ['lot' => $currentlot,
+            'categories_list' => $categories_list,
+            'lotID' => $lotID,
+            'data_list' => $data_list,
+            'rates' => [],
+            'price' => $lot['start_price']+$lot['rate']
+        ]);
+        $con = mysqli_connect('localhost', 'root', '', 'yeticave');
+        mysqli_set_charset($con, 'utf8');
+        $sql = "SELECT categ_id FROM Categories WHERE categ_name='{$lot['category']}'";
+        $result = mysqli_querry($con, $sql);
+        $lot['category'] = mysqli_fetch_assoc($result)['categ_id'];
+
+        $sql = "INSERT INTO lots(lot_name, lot_discr, lot_image, lot_categ_id, lot_first_price, lot_comp_date, lot_step)
+        VALUE ('{$lot['lot_name']}', '{$lot['lot_discr']}', '{$lot['lot_categ_id']}', '{$lot['lot_first_price']}', '{$lot['lot_comp_date']}', '{$lot['lot_step']}')";
+        $result = mysqli_querry($con, $sql);
+        if(!!$result)
+            echo mysqli_error($con);
+
+
     }
 
 }
