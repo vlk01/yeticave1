599ea78dc17fb7cb1bb097ee5e63e7b97c6d3116
vlk01 <79832567+vlk01@users.noreply.github.com>
2021-03-24 16:13:47 +0300

    Практическая 10(1)

---
+++ adds.php
@@ -0,0 +1,69 @@
+<?php
+require_once ('functions.php');
+require 'conBD.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $required_fields = ['lot-name', 'category', 'message', 'lot-rate', 'lot-step', 'lot-date'];
+    $errors = [];
+
+    foreach ($required_fields as $field) {
+        if (empty($_POST[$field])) {
+            $errors[$field] = 'Заполните поле';
+        }
+        if ($field == 'lot-rate') {
+            if (!filter_var($_POST[$field], FILTER_VALIDATE_INT)) {
+                $errors[$field] = 'Начальная цена должна быть корректной';
+            }
+            if (intval($_POST[$field]) <= 0) {
+                $errors[$field] = 'Начальная цена должна быть корректной';
+            }
+        }
+        if ($field == 'lot-step') {
+            if (!filter_var($_POST[$field], FILTER_VALIDATE_INT)) {
+                $errors[$field] = 'Шаг ставки должен быть корректным';
+            }
+            if (intval($_POST[$field]) < 0) {
+                $errors[$field] = 'Начальная цена должна быть корректной';
+            }
+        }
+    }
+    if(count($errors) !== 0) {
+        $page_content = compile_template('add.php',
+            ['errors' => $errors]);
+    } else {
+        $lot = [
+                "image" => file_url ? 'img/user.jpg' : '',
+                "name" => $_POST['lot-name'],
+                "start_price" => $_POST['lot-rate'],
+                "rate" => $_POST['lot-step'],
+                "timer" => $_POST['lot-date'],
+                "category" => $_POST['category'],
+                "description" => $_POST['message'],
+                "account_id" => $_SESSION['auth']['account_id']
+        ];
+    }
+
+}
+
+print_r($lot);
+print_r($errors);
+
+$page_content = compile_template( 'add.php',
+    ['categories_list' => $categories_list,
+        'lot' => $currentLot,
+        'lotID' => $lotID,
+        'data_list' => $data_list,
+        'lot_time_remaining' => $lot_time_remaining]);
+
+$layout_content = compile_template('layout.php',
+    ['page_layout' => 'Главная страница',
+        'is_auth' => $is_auth,
+        'user_avatar' => $user_avatar,
+        'user_name' => $user_name,
+        'page_content' => $page_content,
+        'categories_list' => $categories_list
+    ]);
+
+print($layout_content);
+?>
